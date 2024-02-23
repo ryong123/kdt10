5675a10dbd2b7ae3c26047eb79f14bfab40cd712
seongRyoung <tjdfyd6619@gmail.com>
2024-02-23 13:20:14 +0900

    create shop

---
+++ shop/components/NavComponent.tsx
@@ -0,0 +1,23 @@
+import { Link } from 'react-router-dom';
+export default function NavComponent() {
+  return (
+    <header className="h-10 flex pl-2 sm:pl-10 pr-2 sm:pr-10 flex-wrap">
+      <nav className="flex h-full items-center w-full">
+        <Link to="/">
+          <div>LOGO</div>
+        </Link>
+        <div className="flex justify-end">
+          <Link to="/">
+            <div className="pl-2 sm:pl-10">HOME</div>
+          </Link>
+          <Link to="best">
+            <div className="pl-2 sm:pl-10">SHOP</div>
+          </Link>
+          <Link to="basket">
+            <div className="pl-2 sm:pl-10">BASKET</div>
+          </Link>
+        </div>
+      </nav>
+    </header>
+  );
+}
---
+++ shop/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css';
+import NavComponent from '../components/NavComponent';
+
+function App() {
+  return (
+    <>
+      <NavComponent />
+      <img src="./bg.png" alt="" className=" w-[100%]" />
+
+      <img src="./신발이미지2.jpeg" alt="" />
+    </>
+  );
+}
+
+export default App;
